ea9e99a41d61a89ad1d3f930e8d7f18d179c7eb8
takatokanno <takatokannogamedev@gmail.com>
2021-06-11 12:53:41 +0900

    プロトタイプ宣言とグローバル変数の定義

--- Book-C/Book-C/main.cpp
+++ Book-C/Book-C/main.cpp
@@ -1,4 +1,14 @@
 #include <stdio.h>
+#include <stdlib.h>// �������m�ۗp
+// ����������
+void initialize();
+// �X�V����
+void update();
+// �I������
+void finalize();
+
+char* player = nullptr;// �|�C���^
+
 int main()
 {
 	printf("Hello, world");
