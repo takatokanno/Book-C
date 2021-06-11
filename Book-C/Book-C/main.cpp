15f0872325538d1262c33c2d9330f4a978a642aa
takatokanno <takatokannogamedev@gmail.com>
2021-06-11 12:55:47 +0900

    main.cppをUnicode形式で保存/関数定義

--- Book-C/Book-C/main.cpp
+++ Book-C/Book-C/main.cpp
@@ -1,16 +1,64 @@
-#include <stdio.h>
-#include <stdlib.h>// �������m�ۗp
-// ����������
+﻿#include <stdio.h>
+#include <stdlib.h>// メモリ確保用
+// 初期化処理
 void initialize();
-// �X�V����
+// 更新処理
 void update();
-// �I������
+// 終了処理
 void finalize();
 
-char* player = nullptr;// �|�C���^
+char* player = nullptr;// ポインタ
 
 int main()
 {
-	printf("Hello, world");
+	initialize();
+	update();
+	finalize();
+
 	return 0;
+}
+
+void initialize()
+{
+	// ゲーム開始時の最初の情報を設定する
+	// モデルの読み込み
+	// テクスチャの読み込み
+	// セーブデータの読み込み
+	// プレイヤー情報の設定
+	// エネミー情報の設定
+	// フィールド情報の設定
+	// etc...
+
+	// メモリの確保
+	player = (char*)malloc(sizeof(char) * 10);// char型10個分のメモリを確保する
+}
+
+void update() 
+{
+	// ゲーム起動中の更新処理
+	bool isLoop = true;// bool型は要はフラグ。(true(1)/false(0))の値をとる。(真/偽)
+	char input;
+	while (isLoop)
+	{
+		input = getchar();// 入力待機処理
+
+		// メインループ
+		// qキーが入力されたら終了
+		if (input == 'q') {
+			isLoop = false;
+		}
+
+	}
+}
+
+void finalize() 
+{
+	// ゲーム終了時の処理
+	// モデルデータの解放
+	// テクスチャデータの解放
+	// メモリの解放
+	// etc...
+
+	// メモリの解放
+	free(player);
 }
