24916bb7c791a5a2009217bf98e8279d65cb8b12
takatokanno <takatokannogamedev@gmail.com>
2021-06-26 19:07:34 +0900

    チャプター04”配列とポインタ”の開始、配列からアドレスまで

--- Book-C/Book-C/function.cpp
+++ Book-C/Book-C/function.cpp
@@ -2,3 +2,61 @@
 #include <stdio.h>
 #include <string.h>
 
+//チャプター04"配列とポインタ"
+
+void arrayFactorOfReferenceAndAssignment()
+{
+	int i;
+	int a[] = { 1,2,3,4 };
+
+	for(i = 3; i >= 0; i--)
+		printf("%d ", a[i]);
+	printf("\n");
+}
+
+void replaceArrayCharacter()
+{
+	int i = 0;
+	char a[] = "NET";
+	char b[4];
+
+	while (a[i] != '\0')
+	{
+		b[i] = a[2 - i];
+		i++;
+	}
+	b[3] = '\0';
+	printf("%sは逆から読むと%s\n", a, b);
+}
+
+void getStringLength()
+{
+	char s1[] = "cat", s2[] = "dog";
+	char s[20];
+	sprintf_s(s, "I love %s and %s.", s1, s2);
+	printf("「%s」の文字数は %d\n", s, strlen(s));
+}
+
+void multidimensionalArray()
+{
+	int x, y;
+	int a[2][3] = {
+		{10,20,30},
+		{40,50,60}
+	};
+	for (y = 0; y < 2; y++) {
+		for (x = 0; x < 3; x++)
+			printf("a[%d][%d] = %d ", y, x, a[y][x]);
+		printf("\n");
+	}
+}
+void displayAddress()
+{
+	char a;
+	short b;
+
+	printf("aのアドレスは%p、bのアドレスは%p \n", &a, &b);
+
+}
+
+void 
--- Book-C/Book-C/main.cpp
+++ Book-C/Book-C/main.cpp
@@ -2,7 +2,7 @@
 
 int main()
 {
-	();
+	displayAddress();
 
 	return 0;
 
